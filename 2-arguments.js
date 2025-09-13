c6b205d0cd0a0b1a320916fd00eeaaef738c66a4
Nnanta Charles <nnantaprince2@gmail.com>
2025-09-14 00:46:02 +0100

    functions were created

---
+++ 2-arguments.js
@@ -0,0 +1,17 @@
+
+const forPractice = function(){
+    console.log("No argument");
+}
+forPractice();
+
+const forAlx = function(name){
+    console.log(name + " Argument found");
+
+}
+forAlx("Best");
+
+const forFrontend = function(name, where){
+    console.log(name + " " + where + " Arguments found");
+
+}
+forFrontend("Best", "School");
