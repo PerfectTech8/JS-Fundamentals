fdcc609ed2c6f632afe5493d9c593b24a372f761
Nnanta Charles <nnantaprince2@gmail.com>
2025-09-14 02:09:19 +0100

    all the programms

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,5 +1,8 @@
 
-const weekThree = ["C is fun", "Python is cool", "JavaScript is amazing"];
-for(let i = 0; i < weekThree.length; i++){
-    console.log(weekThree[i]);
-}
+//const weekThree = ["C is fun", "Python is cool", "JavaScript is amazing"];
+//for(let i = 0; i < weekThree.length; i++){
+    //console.log(weekThree[i]);
+
+
+//}
+
