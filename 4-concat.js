c9817e6e524f9bf1bb33e35288ee8c3a9720d682
Nnanta Charles <nnantaprince2@gmail.com>
2025-09-14 14:46:07 +0100

    adding new file

---
+++ 4-concat.js
@@ -0,0 +1,11 @@
+
+const args = process.argv.slice(2);
+if(args[0] && args[1]) {
+    console.log(`${args[0]} is ${args[1]}`);
+}else if (args[0]){
+
+    console.log(`${args[0]} is undefined`);
+    
+}else{
+    console.log("undefined is undefined");
+}
