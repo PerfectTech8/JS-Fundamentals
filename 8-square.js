ebf9af4dd9c663bb2315d01ce93b0c9e8ca04da2
Nnanta Charles <nnantaprince2@gmail.com>
2025-09-14 17:51:26 +0100

    for the update

---
+++ 8-square.js
@@ -0,0 +1,15 @@
+
+const arg = process.argv[2];
+const size = parseInt(arg, 10);
+
+if (isNaN(size)) {
+    console.log("Missing size");
+}else{
+    for (let i = 0; i < size; i++){
+        let line = "";
+        for (let j = 0; j < size; j++){
+            line += "x";
+        }
+        console.log(line);
+    }
+}
