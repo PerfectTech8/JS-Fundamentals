706ee492390467a11042d6a5a4938a415e26564e
Nnanta Charles <nnantaprince2@gmail.com>
2025-09-14 17:09:01 +0100

    updating

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,14 +1,70 @@
 
 const arg = process.argv[2];
-const count = parseInt(arg);
 
-if(isNaN(count)) {
+const count = parseInt(arg, 10);
+if (isNaN(count) || count < 0) {
     console.log("Missing number of occurrences");
-}else{
-    for(let i = 0; i < count; i++){
+}else {
+    for (let i = 0; i < count; i++){
         console.log("C is fun");
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const arg = process.argv[2];
+// const count = parseInt(arg, 10);
+
+// if (isNaN(count)) { 
+//     console.log("Missing number of occurrences");
+// }else {
+//     const messages = Array(count).fill("C is fun");
+//     for (const msg of messages) {
+//         console.log(msg);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const arg = process.argv[2];
+// const count = parseInt(arg);
+
+// if(isNaN(count)) {
+//     console.log("Missing number of occurrences");
+// }else{
+//     for(let i = 0; i < count; i++){
+//         console.log("C is fun");
+//     }
+// }
+
+
+
 
 
 
